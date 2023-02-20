284e55fc5ad39e650d6db559974bdf6428928fee
DUVER ENRIQUE GUTIERREZ AVILA <duver.gutierrez@correo.policia.gov.co>
2023-02-20 09:10:08 -0500

    Ejercicio 3 finalizado

--- MediaEstadistica.py
+++ MediaEstadistica.py
@@ -3,14 +3,17 @@ y este cree un grupo de números de forma aleatoria (enteros positivos), los gua
 en una lista y muestre por pantalla su media, suma, valor mínimo y máximo, y su mediana."""
 
 import random
+import math
+from os import system
 
 vectorInicial = []
 vectorOrdenado = []
 numMin = 0
 
 def GenerarNumeros(num):
+    system("cls")
     for item in range(num):
-        vectorInicial.append(random.randint(1, 100))
+        vectorInicial.append(random.randint(1, 10))
     
     OrdenarArray()
 
@@ -21,11 +24,48 @@ def CalcularMedia(vectorOrdenado):
     media = sumaItems / len(vectorOrdenado)
     print(f'Media: {media}')
 
+def CalcularSuma(vectorOrdenado):
+    sumaItems = 0
+    for item in range(len(vectorOrdenado)):
+        sumaItems += item
+    print(f'Suma: {sumaItems}')
+
+def CalcularMediana(vectorOrdenado):
+    cantidad = len(vectorOrdenado)
+    
+    if cantidad % 2 == 0:
+        mitad = int(cantidad/2)
+        suma = (vectorOrdenado[mitad-1] + vectorOrdenado[mitad])/2
+        if suma - math.trunc(suma) == 0:
+            print(f'Mediana: {math.trunc(suma)}')
+        else:
+            print(f'Mediana: {suma}')
+    else:
+        mitad = int((cantidad - 1)/2)
+        print(f'Mediana: {vectorOrdenado[mitad]}')
+            
+
+def CalcularMinimo(vectorOrdenado):
+    numMin = vectorOrdenado[0]
+
+    for item in range(len(vectorOrdenado)):
+        if vectorOrdenado[item] < numMin:
+                numMin = vectorOrdenado[item]
+    print(f'numero menor: {numMin}')
+
+def CalcularMayor(vectorOrdenado):
+    numMax = vectorOrdenado[0]
+    
+    for item in range(len(vectorOrdenado)):
+        if vectorOrdenado[item] > numMax:
+                numMax = vectorOrdenado[item]
+    print(f'numero mayor: {numMax}')
+
+
 def OrdenarArray():
     print(f'vectorInicial {vectorInicial}')
 
     global numMin
-
     numMin = vectorInicial[0]
 
     for item in range(len(vectorInicial)):
@@ -34,17 +74,21 @@ def OrdenarArray():
 
             if vectorInicial[item1] < numMin:
                 numMin = vectorInicial[item1]
-        print(f'numero menor: {numMin}')
         vectorInicial.remove(numMin)
         vectorOrdenado.append(numMin)
         
         if len(vectorInicial) > 0:
             numMin = vectorInicial[0]
 
-        print(f'vectorInicial {vectorInicial}')
-        print(f'vectorOrdenado {vectorOrdenado}')
-        print('')
+    print(f'vectorOrdenado {vectorOrdenado}')
+    print('')    
     
     CalcularMedia(vectorOrdenado)
+    CalcularSuma(vectorOrdenado)
+    CalcularMinimo(vectorOrdenado)
+    CalcularMayor(vectorOrdenado)    
+    CalcularMediana(vectorOrdenado)
+
+
 
 num = GenerarNumeros(int(input("Indique cantidad de numeros a generar: ")))
