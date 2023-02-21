6cfffb99e81d146b0273ff0308f3e7db4db6379c
Duver Gutierrez <degutierrez33@misena.edu.co>
2023-02-20 22:41:43 -0500

    Ajuste ejercicio MCM

--- MCMyMCD.py
+++ MCMyMCD.py
@@ -4,11 +4,32 @@
 import random
 from os import system
 
+def OrdenarArray(vectorInicial):
+    vectorOrdenado = []
+    print(f'vectorInicial {vectorInicial}')
+
+    global numMin
+    numMin = vectorInicial[0]
+
+    for item in range(len(vectorInicial)):
+        for item1 in range(len(vectorInicial)):
+
+            if vectorInicial[item1] < numMin:
+                numMin = vectorInicial[item1]
+        vectorInicial.remove(numMin)
+        vectorOrdenado.append(numMin)
+        
+        if len(vectorInicial) > 0:
+            numMin = vectorInicial[0]
+
+    print(f'vectorOrdenado {vectorOrdenado}')
+    return vectorOrdenado
+    
+
 def RecorrerNumeros(num):
     vectorValores = []
 
     for item in range(2, num+1):
-        #print(f'Posicion: {item}')
         
         if VerificarPrimo(item):
             vectorValores.append(item)
@@ -35,13 +56,66 @@ def DescomponerNum(num, vector):
     
     return vectorFactores
 
-#num = RecorrerNumeros(int(input("digite numero:  ")))
+def GenerarMultiplos(num, cant):
+    vectorMultiplos = []
+    for item in range(1, cant*10):
+        vectorMultiplos.append(num * item)    
+    return vectorMultiplos
+
+def CompararListas(lista1, lista2, lista3):
+    for item in range(len(lista1)):
+        item1 = lista1[item]
+        array2 = list(filter(lambda x : x == item1, lista2))
+        array3 = list(filter(lambda x : x == item1, lista3))
+        if len(array2) > 0 and len(array3) > 0:
+            item2 = array2[0]
+            item3 = array3[0]
+            if item1 == item2 and item2 == item3:
+                print(f'Multiplos encontrados: {item1} - {item2} - {item3}')
+                return item3
 
 system("cls")
 
 num1 = int(input('ingrese primer numero: '))
+num2 = int(input('ingrese segundo numero: '))
+num3 = int(input('ingrese tercer numero: '))
+print('')
+
+vectoresIngresados = [num1, num2, num3]
+vectoresIngresados = OrdenarArray(vectoresIngresados)
+num1 = vectoresIngresados[2]
+num2 = vectoresIngresados[1]
+num3 = vectoresIngresados[0]
+
 vector1 = RecorrerNumeros(num1)
-factor2 = DescomponerNum(num1, vector1)
+factor1 = DescomponerNum(num1, vector1)
 print(f'Primos vector 1 del numero {num1}: {vector1}')
-print(f'Factores 2: {factor2}')
+print(f'Factores vector 1: {factor1}')
+print('')
+
+vector2 = RecorrerNumeros(num2)
+factor2 = DescomponerNum(num2, vector2)
+print(f'Primos vector 1 del numero {num2}: {vector2}')
+print(f'Factores vector 1: {factor2}')
+print('')
+
+vector3 = RecorrerNumeros(num3)
+factor3 = DescomponerNum(num3, vector3)
+print(f'Primos vector 1 del numero {num3}: {vector3}')
+print(f'Factores vector 1: {factor3}')
+print('')
+
+lista1 = GenerarMultiplos(num1, num1)
+print(f'Multiplo numero 1: {lista1}')
+
+lista2 = GenerarMultiplos(num2, num1)
+print(f'Multiplo numero 2: {lista2}')
+
+lista3 = GenerarMultiplos(num3, num1)
+print(f'Multiplo numero 3: {lista3}')
+
+mcm = CompararListas(lista1, lista2, lista3)
+print(f'el MCM es: {mcm}')
+
+
 
